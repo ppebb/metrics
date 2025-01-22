2510b93731a3ce4c6e53e7781970deac21dd8d17
ppeb <ppeb@ppeb.me>
2025-01-22 01:58:32 -0600

    Move config to config.go, add filters for repo names

---
+++ config.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"slices"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Location     string
+	Indepth      bool
+	Countloc     bool
+	Token        string
+	ExcludeForks bool
+	Users        []string
+	Orgs         []string
+	Repositories []string
+	Authors      []string
+	Filters      []string
+	Ignore       struct {
+		Vendor        bool
+		Dotfiles      bool
+		Configuration bool
+		Image         bool
+		Test          bool
+		Binary        bool
+		Generated     bool
+	}
+}
+
+var outputPath string
+var config Config
+var reposToCheck []string
+
+func config_init(path string) {
+	data, err := os.ReadFile(path)
+	check(err)
+
+	config = Config{}
+	err = yaml.Unmarshal(data, &config)
+	check(err)
+
+	check_field(len(config.Location), "location")
+	check_field(len(config.Repositories), "repositories")
+	check_field(len(config.Authors), "authors")
+
+	err = os.MkdirAll(config.Location, os.FileMode(0777))
+	check(err)
+
+	reposToCheck = config.Repositories
+
+	var testRepo func(repo string) bool
+
+	if len(config.Filters) > 0 {
+		filters := []*regexp.Regexp{}
+
+		for _, pattern := range config.Filters {
+			regex, err := regexp.Compile(pattern)
+
+			if err != nil {
+				panic(fmt.Sprintf("Pattern \"%s\" failed to compile to regex: error %s", pattern, err.Error()))
+			}
+
+			filters = append(filters, regex)
+		}
+
+		testRepo = func(repo string) bool {
+			for _, regex := range filters {
+				if regex.MatchString(repo) {
+					return true
+				}
+			}
+
+			return false
+		}
+	} else {
+		testRepo = func(_ string) bool { return false }
+	}
+
+	copyToReposToCheck := func(repoResponses []RepoResponse) {
+		for _, repo := range repoResponses {
+			if config.ExcludeForks && repo.Fork {
+				continue
+			}
+
+			if testRepo(repo.Full_Name) {
+				continue
+			}
+
+			if !slices.Contains(reposToCheck, repo.Full_Name) {
+				reposToCheck = append(reposToCheck, repo.Full_Name)
+			}
+		}
+	}
+
+	for _, user := range config.Users {
+		copyToReposToCheck(github_get_account_repos(user, false, config.Token))
+	}
+
+	for _, org := range config.Orgs {
+		copyToReposToCheck(github_get_account_repos(org, true, config.Token))
+	}
+}
--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"slices"
-
-	"gopkg.in/yaml.v3"
 )
 
 func check(e error) {
@@ -19,31 +16,6 @@ func check_field(l int, name string) {
 		panic(fmt.Sprintf("Config is missing field %s!", name))
 	}
 }
-
-type Config struct {
-	Location     string
-	Indepth      bool
-	Countloc     bool
-	Token        string
-	ExcludeForks bool
-	Users        []string
-	Orgs         []string
-	Repositories []string
-	Authors      []string
-	Ignore       struct {
-		Vendor        bool
-		Dotfiles      bool
-		Configuration bool
-		Image         bool
-		Test          bool
-		Binary        bool
-		Generated     bool
-	}
-}
-
-var outputPath string
-var config Config
-var dryRun = false
 
 func print_help() {
 	fmt.Printf(`
@@ -61,6 +33,7 @@ Usage: ./ppebtrics [OPTIONS]
 
 func main() {
 	var config_path string
+	var dryRun = false
 
 	argsLen := len(os.Args)
 	for i := 1; i < argsLen; i++ {
@@ -90,41 +63,7 @@ func main() {
 		panic("Missing config argument, provide a config.yml with -c or --config")
 	}
 
-	data, err := os.ReadFile(config_path)
-	check(err)
-
-	config = Config{}
-	err = yaml.Unmarshal(data, &config)
-	check(err)
-
-	check_field(len(config.Location), "location")
-	check_field(len(config.Repositories), "repositories")
-	check_field(len(config.Authors), "authors")
-
-	err = os.MkdirAll(config.Location, os.FileMode(0777))
-	check(err)
-
-	reposToCheck := config.Repositories
-
-	copyToReposToCheck := func(repoResponses []RepoResponse) {
-		for _, repo := range repoResponses {
-			if config.ExcludeForks && repo.Fork {
-				continue
-			}
-
-			if !slices.Contains(reposToCheck, repo.Full_Name) {
-				reposToCheck = append(reposToCheck, repo.Full_Name)
-			}
-		}
-	}
-
-	for _, user := range config.Users {
-		copyToReposToCheck(github_get_account_repos(user, false, config.Token))
-	}
-
-	for _, org := range config.Orgs {
-		copyToReposToCheck(github_get_account_repos(org, true, config.Token))
-	}
+	config_init(config_path)
 
 	if dryRun {
 		fmt.Println("The following repositories will be cloned and analyzed:")
